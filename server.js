2467db262cf113ae4821abab62c3cf0c4e56c072
BenzTEST <55734110+BenzPG@users.noreply.github.com>
2021-01-12 04:14:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var port = process.env.PORT || 9000;
 app.get("/", (req, res) => {
   res.json({ message: "Ahoy!" });
 });
+app.get("/v1/test", (req, res) => {
+  res.json({ message: "test!" });
+});
 
 app.listen(port, () => {
   console.log("Application is running on port "+port);
